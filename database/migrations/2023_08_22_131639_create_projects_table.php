31fd92f669cf200fa90b5c3c2f59df332672e4fe
Eligio Biasi <ex_98@live.it>
2023-08-22 15:59:05 +0200

    used faker seed and populate the db

--- database/migrations/2023_08_22_131639_create_projects_table.php
+++ database/migrations/2023_08_22_131639_create_projects_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->string('slug');
+            $table->text('image');
             $table->timestamps();
         });
     }
