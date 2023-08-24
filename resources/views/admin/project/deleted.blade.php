1dee95b54833cf1f4a2c79e5973deeb7efba5553
Eligio Biasi <ex_98@live.it>
2023-08-24 15:28:18 +0200

    small test change

--- resources/views/admin/project/deleted.blade.php
+++ resources/views/admin/project/deleted.blade.php
@@ -4,6 +4,9 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-12">
+          <h1>
+            small test change
+          </h1>
             <table class="table table-bordered border-primary">
                 <thead>
                     <tr>
